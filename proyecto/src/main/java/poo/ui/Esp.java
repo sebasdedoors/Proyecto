3231ab8ce6ebec19dab4370fd4fce6f926f7960c
sebasdedoors <sebastian28_04@hotmail.com>
2025-02-07 12:39:31 -0600

    Idiomas 2

--- proyecto/src/main/java/poo/ui/Esp.java
+++ proyecto/src/main/java/poo/ui/Esp.java
@@ -1,5 +1,26 @@
 package poo.ui;
 
-public class Esp {
+public class Esp extends Lang{
+    public Esp(){
+        EXIT = "Gracias por visitarnos ;)...";
+        MENU = "\u001B[1;34m"+"|             Menu:             |\n|      1.Agregar un libro.     |\n|    2. Agregar un usuario.    |\n|      3. Mostrar libros.      |\n|     4. Mostrar usuarios.     |\n|           5.Salir.           |"+"\u001B[0m";
+        INVALID = "Opción no valida, por favor intente de nuevo.";
+        ADDBOOK = "Escriba el nombre del libro a agregar:";
+        ADDUSER = "Escribe el nombre del usuario por favor:";
+        NOBOOKS = "No hay libros en la lista.";
+        NOUSERS = "No hay usuarios en la lista.";
+        GOODBOOK = "El libro agregado excistosamente.";
+        ERRBOOK = "Error al agregar el libro: ";
+        GENERBOOK = "Escriba el genero el libro por favor:";
+        AGEBOOK = "Escribe la edad recomendada del libro:";
+        STATUSUSER = "Escriba el estado del usuario por favor:";
+        GOODUSER = "El usuario se agregó con exito.";
+        ERRUSER = "Error al agregar el usuario: ";
+        USER = "Nombre: ";
+        STATUS = "Estado: ";
+        TITLE = "Titulo: ";
+        GENER = "Genero: ";
+        AGE = "Edad recomandada: ";
+    }
 
 }
---
+++ proyecto/src/main/java/poo/ui/Lang.java
@@ -0,0 +1,23 @@
+package poo.ui;
+
+public class Lang {
+    protected String EXIT;
+    protected String MENU;
+    protected String INVALID;
+    protected String ADDBOOK;
+    protected String ADDUSER;
+    protected String NOBOOKS;
+    protected String NOUSERS;
+    protected String GOODBOOK;
+    protected String ERRBOOK;
+    protected String GENERBOOK;
+    protected String AGEBOOK;
+    protected String STATUSUSER;
+    protected String GOODUSER;
+    protected String ERRUSER;
+    protected String USER;
+    protected String STATUS;
+    protected String TITLE;
+    protected String GENER;
+    protected String AGE;
+}
