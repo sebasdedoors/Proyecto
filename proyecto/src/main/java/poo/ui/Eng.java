64ea5ba943b1f7b08438803bc765ec781e75ca1a
michelglez <al03001350@tecmilenio.mx>
2025-02-07 12:53:20 -0600

    Commit Ingles

--- proyecto/src/main/java/poo/ui/Eng.java
+++ proyecto/src/main/java/poo/ui/Eng.java
@@ -1,5 +1,26 @@
 package poo.ui;
 
-public class Eng {
+public class Eng extends Lang{
+    public Eng(){
 
+    EXIT = "Thank you for visiting us ;)...";
+    MENU = "\u001B[1;34m"+"|             Menu:             |\n|      1. Add a book.          |\n|    2. Add a user.            |\n|      3. Show books.          |\n|     4. Show users.           |\n|           5. Exit.           |"+"\u001B[0m";
+    INVALID = "Invalid option, please try again.";
+    ADDBOOK = "Enter the name of the book to add:";
+    ADDUSER = "Enter the user's name, please:";
+    NOBOOKS = "There are no books in the list.";
+    NOUSERS = "There are no users in the list.";
+    GOODBOOK = "The book was successfully added.";
+    ERRBOOK = "Error adding the book: ";
+    GENERBOOK = "Enter the book's genre, please:";
+    AGEBOOK = "Enter the recommended age for the book:";
+    STATUSUSER = "Enter the user's status, please:";
+    GOODUSER = "The user was successfully added.";
+    ERRUSER = "Error adding the user: ";
+    USER = "Name: ";
+    STATUS = "Status: ";
+    TITLE = "Title: ";
+    GENER = "Genre: ";
+    AGE = "Recommended age: ";
+    }
 }
