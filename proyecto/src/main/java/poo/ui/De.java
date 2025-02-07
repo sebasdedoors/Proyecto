998e6889802773fb3b1399c2b27618684df371f1
danielaliz1 <daniela.liz.real@outlook.com>
2025-02-07 12:49:31 -0600

    Commit Alemán

--- proyecto/src/main/java/poo/ui/De.java
+++ proyecto/src/main/java/poo/ui/De.java
@@ -1,4 +1,28 @@
 package poo.ui;
 
-public class De{
+public class De extends Lang{
+    public De(){
+
+        EXIT = "Danke für Ihren Besuch ;)...";
+        MENU = "\u001B[1;31m"+"|             Menü:             |\n|    1. Ein Buch hinzufügen.   |\n| 2. Einen Benutzer hinzufügen.|\n|      3. Bücher anzeigen.     |\n|     4. Benutzer anzeigen.    |\n|          5. Beenden.         |"+"\u001B[0m";
+        INVALID = "Ungültige Option, bitte versuchen Sie es erneut.";
+        ADDBOOK = "Geben Sie den Namen des Buches ein, das hinzugefügt werden soll:";
+        ADDUSER = "Geben Sie den Namen des Benutzers ein, bitte:";
+        NOBOOKS = "Es gibt keine Bücher in der Liste.";
+        NOUSERS = "Es gibt keine Benutzer in der Liste.";
+        GOODBOOK = "Das Buch wurde erfolgreich hinzugefügt.";
+        ERRBOOK = "Fehler beim Hinzufügen des Buches: ";
+        GENERBOOK = "Geben Sie das Genre des Buches ein, bitte:";
+        AGEBOOK = "Geben Sie das empfohlene Alter für das Buch ein:";
+        STATUSUSER = "Geben Sie den Status des Benutzers ein, bitte:";
+        GOODUSER = "Der Benutzer wurde erfolgreich hinzugefügt.";
+        ERRUSER = "Fehler beim Hinzufügen des Benutzers: ";
+        USER = "Name: ";
+        STATUS = "Status: ";
+        TITLE = "Titel: ";
+        GENER = "Genre: ";
+        AGE = "Empfohlenes Alter: ";
+        
+    
+    }
 }
